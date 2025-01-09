d4f5d8515402951c0b23d836f4ca57a7c00017ed
Carlos Szpiro <carlos.szpiro@outlook.com>
2025-01-09 01:10:02 -0300

    file: Alteração do min-w-[500] para w-[500]

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -11,7 +11,7 @@ function TaskPage() {
 
   return (
     <div className="w-screen  h-screen bg-slate-500 flex justify-center p-6">
-      <div className="min-w-[500px] space-y-4">
+      <div className="w-[500px] space-y-4">
         
         <div className="flex items-center justify-center relative">
           <button
